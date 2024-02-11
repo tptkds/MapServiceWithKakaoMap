11e8ece1de33a9e996306f1171314e4dc08c548d
KimYouGyoung <tptkds12@gmail.com>
2024-02-11 23:20:18 +0900

    카카오 지도 화면 가져오기 완료

--- script.js
+++ script.js
@@ -31,6 +31,12 @@
     return document.querySelector(target)
   }
 
+  const $map = get('#map');
+  const mapContainer = new kakao.maps.Map($map, {
+    center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+    level: 3 //지도의 레벨(확대, 축소 정도)
+  })
+
   const init = () => {}
 
   init()
